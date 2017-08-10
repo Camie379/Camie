4e003e250e9f706b33d1e41a6a38ac5c2ebbacae
simp31231 <simp31231@gmail.com>
2017-08-10 02:26:16 -0400

    first commit

---
+++ static/pong/offline.js
@@ -0,0 +1,20 @@
+﻿{
+	"version": 1502085412,
+	"fileList": [
+		"data.js",
+		"c2runtime.js",
+		"jquery-2.1.1.min.js",
+		"offlineClient.js",
+		"images/bara-sheet0.png",
+		"images/ball-sheet0.png",
+		"images/wall-sheet0.png",
+		"images/addscoreb-sheet0.png",
+		"images/txtscoreb.png",
+		"icon-16.png",
+		"icon-32.png",
+		"icon-114.png",
+		"icon-128.png",
+		"icon-256.png",
+		"loading-logo.png"
+	]
+}
